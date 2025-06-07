99b9129e1e2e39667d693f59cf9ccc477f852b29
urkiddennn <154726035+urkiddennn@users.noreply.github.com>
2025-06-07 16:28:22 +0800

    Update: Selecting random words

--- main.go
+++ main.go
@@ -1,7 +1,75 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/pterm/pterm"
+)
+
+// For getting the current time and date
+type (
+	TimeStamp time.Time
+	Highest   struct {
+		highestWPM int
+		HighestAcc float64
+	}
+)
+
+type typeHistory struct {
+	id          int
+	scoreWMP    int
+	accuracyWPM float64
+	Created_at  *TimeStamp
+}
 
 func main() {
-	fmt.Println("Hello everyone")
+	pterm.Info.Println("Select Menu")
+
+	for {
+		choice, _ := pterm.DefaultInteractiveSelect.WithOptions([]string{"Home", "Profile", "Exit"}).Show("Select Where you want to go")
+		switch choice {
+		case "Home":
+			displayHome()
+		}
+	}
+}
+
+func displayHome() string {
+	selectedDef, _ := pterm.DefaultInteractiveSelect.WithOptions([]string{"Easy", "Medium", "Hard"}).Show("Select Difficulty")
+
+	switch selectedDef {
+	case "Easy":
+		selectRandomWords()
+	}
+	return selectedDef
+}
+
+func selectRandomWords() {
+	easyLevel := 10
+	// easyLevelTime := 3 * time.Second
+
+	// gameStart := false
+	words := []string{
+		"apple", "breeze", "cactus", "dolphin", "eagle",
+		"fossil", "glacier", "horizon", "island", "jigsaw", "kitten", "lantern",
+		"mosaic", "nectar", "oasis", "puzzle", "quartz", "river", "shadow",
+		"tiger", "umbrella", "violet", "whisper", "xylophone", "yogurt",
+		"zebra", "anchor", "blizzard", "canyon", "desert", "emerald",
+		"flame", "guitar", "hammock", "indigo", "jungle", "kayak",
+		"lotus", "meadow", "noodle", "orchid", "pebble", "quilt",
+		"rainbow", "sapphire", "tulip", "vortex", "willow",
+		"zenith", "compass", "are", "you", "and", "how", "welcome", "subscribe",
+	}
+
+	rand.Shuffle(len(words), func(i, j int) {
+		words[i], words[j] = words[j], words[i]
+	})
+	selectedRandomWords := words[:easyLevel]
+
+	fmt.Println("Selected words", selectedRandomWords)
+}
+
+func GameStart() {
 }
